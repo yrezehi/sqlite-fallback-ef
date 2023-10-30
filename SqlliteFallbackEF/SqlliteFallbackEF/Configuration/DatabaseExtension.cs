43579aadb70a37fda4b9fb1ee9db79bdf5a11662
unknown <yrezehi@gmail.com>
2023-10-30 08:27:00 -0700

    setup: refs

---
+++ SqlliteFallbackEF/SqlliteFallbackEF/Configuration/DatabaseExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlliteFallbackEF.Configuration
+{
+    public static class DatabaseExtension
+    {
+        public static void RegisterFallback(this WebApplicationBuilder application)
+        {
+
+        }
+    }
+}
