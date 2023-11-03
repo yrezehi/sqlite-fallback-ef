b37f9abe12936e65898e8e35b8dfbe7b326f9dca
unknown <yrezehi@gmail.com>
2023-11-02 20:39:52 -0700

    setup: SqliteFallbackEF.Sample

---
+++ SqlliteFallbackEF/SqliteFallbackEF.Sample/ControllerRegistry.cs
@@ -0,0 +1,11 @@
+﻿namespace SqliteFallbackEF.Sample
+{
+    public static class ControllerRegistry
+    {
+        public static void RegisterControllers(this WebApplication application) =>
+            application.Index();
+
+        private static void Index(this WebApplication application) =>
+            application.MapGet("/", () => Results.Ok());
+    }
+}
