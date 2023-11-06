add14cb544e7c441f305a4cba7bc063a15c0b236
unknown <yrezehi@gmail.com>
2023-11-05 16:17:54 -0800

    modify: DatabaseExceptionInterceptor

--- SqlliteFallbackEF/SqliteFallbackEF.Sample/Services/EntitiesService.cs
+++ SqlliteFallbackEF/SqliteFallbackEF.Sample/Services/EntitiesService.cs
@@ -1,10 +1,24 @@
-﻿using SqliteFallbackEF.Sample.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SqliteFallbackEF.Sample.Models;
 using SqliteFallbackEF.Sample.Repositories.Abstracts.Interfaces;
 
 namespace SqliteFallbackEF.Sample.Services
 {
-    public class EntitiesService : ServiceBase<Entity>
+    public class EntitiesService
     {
-        public EntitiesService(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+        protected internal IUnitOfWork UnitOfWork { get; set; }
+        protected DbSet<Entity> DBSet { get; set; }
+
+        public EntitiesService(IUnitOfWork unitOfWork) =>
+            (DBSet, UnitOfWork) = (unitOfWork.Repository<Entity>().DBSet, unitOfWork);
+
+        public async Task<Entity> Create(Entity entity)
+        {
+            await DBSet.AddAsync(entity);
+
+            await UnitOfWork.CompletedAsync();
+
+            return entity;
+        }
     }
 }
--- SqlliteFallbackEF/SqliteFallbackEF/Interceptors/DatabaseExceptionInterceptor.cs
+++ SqlliteFallbackEF/SqliteFallbackEF/Interceptors/DatabaseExceptionInterceptor.cs
@@ -5,17 +5,9 @@ namespace SqliteFallbackEF.Interceptors
 {
     public class DatabaseExceptionInterceptor : DbCommandInterceptor
     {
-        public override async Task<InterceptionResult<DbDataReader>> ReaderExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result, CancellationToken cancellationToken = default)
+        public override void CommandFailed(DbCommand command, CommandErrorEventData eventData)
         {
-            try
-            {
-                return await base.ReaderExecutingAsync(command, eventData, result, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                
-                return result;
-            }
+
         }
     }
 }
